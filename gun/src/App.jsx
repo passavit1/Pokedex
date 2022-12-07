6c2980411e85d1652b5dbabd18a2b76e4b9edcec
Wizat <passavit1@gmail.com>
2022-12-07 23:32:23 +0700

    [02-setup]-install ui dependency

---
+++ gun/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <div>React App</div>
+      <Button type="primary">Click</Button>
+    </div>
+  );
+}
+
+export default App;

f465b4ac2278cf0c78e8360a35514e132074e901
Wizat <passavit1@gmail.com>
2022-12-08 23:25:50 +0700

    [02-setup] - restyling and setup background

--- gun/src/App.jsx
+++ gun/src/App.jsx
@@ -1,10 +1,13 @@
-import { Button } from 'antd';
+import { Button, Image } from 'antd';
+
+import logoSRC from './assets/images/pokedex.png';
 
 function App() {
   return (
     <div>
       <div>React App</div>
       <Button type="primary">Click</Button>
+      <Image width={200} src={logoSRC} />
     </div>
   );
 }
